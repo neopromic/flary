120720ca3c0a0223349a83c5d8b33df085b61c6b
Chroni <chronigraal@gmail.com>
2024-12-20 23:01:42 -0300

    feat: neo is a calvo

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('NeO é careca!!!'),
+          child: Text('NeO é careca!!'),
         ),
       ),
     );
