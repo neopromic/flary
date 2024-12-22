3fd6b7555d8ee73c615f7d09251eb2099bf81f4a
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-12-22 10:59:14 -0300

    feat: add onboarding screen with navigation to signup page

---
+++ lib/screens/onboarding/page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  const Text(
+                    'Conheça o Flary',
+                    style: TextStyle(
+                      fontSize: 32,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child: const Text(
+                      'Prosseguir',
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
