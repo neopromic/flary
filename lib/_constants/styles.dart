c4035da978a6d34b0456b321390d1630da07dde8
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-12-23 16:08:22 -0300

    feat: add constants

---
+++ lib/_constants/styles.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class AppColors {
+  static const Color primary = Color(0xFF7238BA); // Roxo opaco
+  static const Color secondary = Color(0xFF03DAC5); // Turquesa
+  static const Color background = Color(0xFF1B161F); // Preto com tom púrpura
+  static const Color surface = Color(0xFFFFFFFF); // Branco opaco
+  static const Color error = Color(0xFFB00020); // Vermelho opaco
+  static const Color textPrimary = Color(0xFFFFFFFF); // Texto branco
+  static const Color textSecondary =
+      Color(0xFF757575); // Cinza claro para textos secundários
+}
